1fb60ad29d0e8403913d511375088b6741627e4e
yzdrf98@mail.ru <yzdrf98@mail.ru>
2017-04-12 19:00:16 +0400

    cookie <yzdrf98@mail.ru>

---
+++ cratch/Cookie/Cookies.php
@@ -0,0 +1,54 @@
+<?php
+namespace Cratch\Cookie;
+
+use Cratch\Cookie\Hash;
+use Cratch\Contracts\Cookie\CookieInterface;
+
+class Cookies implements CookieInterface
+{
+    /**
+     * @param string $key
+     * @param string $value
+     * @param int $time
+     * @return $this
+     */
+    public function set(string $key, string $value, int $time = 1)
+    {
+        $value = Hash::encrypt($value);
+        setcookie($key, $value, time() + $time * 60 * 60);
+        return $this;
+    }
+
+    /**
+     * @param string $key
+     * @return bool|string
+     */
+    public function get (string $key): bool
+    {
+        if ($this->has ($key)) {
+            return Hash::decrypt($_COOKIE[$key]);
+        }
+        return false;
+    }
+
+    /**
+     * @param string $key
+     * @return bool
+     */
+    public function remove (string $key): bool
+    {
+        if ($this->has ($key)) {
+            unset($_COOKIE[$key]);
+        }
+        return false;
+    }
+
+    /**
+     * @param string $key
+     * @return bool
+     */
+    public function has (string $key): bool
+    {
+        return $_COOKIE[$key] ?? false;
+    }
+}
