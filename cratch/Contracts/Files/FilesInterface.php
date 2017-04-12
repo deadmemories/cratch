cf7e4e521334a13a687be450ee765e053ffa7ae8
yzdrf98@mail.ru <yzdrf98@mail.ru>
2017-04-12 23:37:58 +0400

    files <yzdrf98@mail.ru>

---
+++ cratch/Contracts/Files/FilesInterface.php
@@ -0,0 +1,17 @@
+<?php
+namespace Cratch\Contracts\Files;
+
+interface FilesInterface
+{
+    public function get ($path);
+    public function put ($path, $content);
+    public function append ($path, $content);
+    public function upload ($path, $file);
+    public function extension ($file);
+    public function is ($type, $file);
+    public function mime ($type);
+    public function delete ($path);
+    public function newDir ($name);
+    public function dropDir ($name);
+    public function move ($file, $to, $name);
+}
