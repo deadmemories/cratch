0adb041826215702c4241c19a5b1e554dbf84aec
DeadMemories <deadmoras5@gmail.com>
2017-04-13 17:08:11 +0300

    update tests and support/functions.php

--- src/cratch/Support/Functions.php
+++ src/cratch/Support/Functions.php
@@ -1,39 +1,51 @@
 <?php
 
-function app()
-{
-    return \Cratch\Container\ServiceContainer::getInstance();
+if (! function_exists('app')) {
+    function app()
+    {
+        return \Cratch\Container\ServiceContainer::getInstance();
+    }
 }
 
-function collection(array $items = [])
-{
-    app()->setParams(
-        'collection', [
-            'items' => $items,
-        ]
-    );
+if (! function_exists('collection')) {
+    function collection(array $items = [])
+    {
+        app()->setParams(
+            'collection', [
+                'items' => $items,
+            ]
+        );
 
-    return app()->make('collection');
+        return app()->make('collection');
+    }
 }
 
-function cookie()
-{
-    return app()->make('cookie');
+if (! function_exists('cookie')) {
+    function cookie()
+    {
+        return app()->make('cookie');
+    }
 }
 
-function mainConfig()
-{
-    return app()->make('mainConfig');
+if (! function_exists('mainConfig')) {
+    function mainConfig()
+    {
+        return app()->make('mainConfig');
+    }
 }
 
-function userConfig()
-{
-    return app()->make('userConfig');
+if (! function_exists('userConfig')) {
+    function userConfig()
+    {
+        return app()->make('userConfig');
+    }
 }
 
-function dd()
-{
-    $args = func_get_args();
-    call_user_func_array('dump', $args);
-    die();
+if (! function_exists('dd')) {
+    function dd()
+    {
+        $args = func_get_args();
+        call_user_func_array('dump', $args);
+        die();
+    }
 }
--- tests/Container/ContainerTest.php
+++ tests/Container/ContainerTest.php
@@ -6,37 +6,43 @@ use PHPUnit\Framework\TestCase;
 
 class ContainerTest extends TestCase
 {
-    /**
-     * @var \Cratch\Container\ServiceContainer
-     */
-    private $container;
 
     public function setUp()
     {
         parent::setUp();
 
-        $this->container = \Cratch\Container\ServiceContainer::getInstance();
+        app()->register('collection', \Cratch\Collection\Collection::class);
     }
 
     public function testRegister()
     {
-        $this->assertNull($this->container->register('SomeClass', stdClass::class));
-        $this->assertNull($this->container->register(\Cratch\Config\Config::class));
+        $this->assertNull(
+            app()->register('SomeClass', stdClass::class)
+        );
+
+        $this->assertNull(
+            app()->register(\Cratch\Config\Config::class)
+        );
     }
 
     public function testRegisterSingleton()
     {
-        $this->assertNull($this->container->registerSingleton('SomeClass1', stdClass::class));
-        $this->assertNull($this->container->registerSingleton(\Cratch\Config\UserConfig::class));
+        $this->assertNull(
+            app()->registerSingleton('SomeClass1', stdClass::class)
+        );
+
+        $this->assertNull(
+            app()->registerSingleton(\Cratch\Config\UserConfig::class)
+        );
     }
 
     public function testHasClasses()
     {
-        $this->assertFalse($this->container->has('alias_name'));
-        $this->assertFalse($this->container->has(\Cratch\Support\Files::class));
+        $this->assertFalse(app()->has('alias_name'));
+        $this->assertFalse(app()->has(\Cratch\Support\Files::class));
 
-        $this->assertTrue($this->container->has('SomeClass'));
-        $this->assertTrue($this->container->has(\Cratch\Config\Config::class));
+        $this->assertTrue(app()->has('SomeClass'));
+        $this->assertTrue(app()->has(\Cratch\Config\Config::class));
     }
 
     /**
@@ -44,14 +50,25 @@ class ContainerTest extends TestCase
      */
     public function testMake()
     {
-        $this->assertInstanceOf(stdClass::class, $this->container->make('SomeClass1'));
-        $this->assertInstanceOf(\Cratch\Config\Config::class, $this->container->make(\Cratch\Config\Config::class));
+        $this->assertInstanceOf(
+            stdClass::class,
+            app()->make('SomeClass1')
+        );
+
+        $this->assertInstanceOf(
+            \Cratch\Config\Config::class,
+            app()->make(\Cratch\Config\Config::class)
+        );
     }
 
     public function testSetParams()
     {
-        $this->assertNull($this->container->setParams(\Cratch\Config\Config::class, [
-            \Cratch\Contracts\Config\ConfigInterface::class => \Cratch\Config\UserConfig::class
-        ]));
+        $this->assertNull(
+            app()->setParams(
+                \Cratch\Config\Config::class, [
+                    \Cratch\Contracts\Config\ConfigInterface::class => \Cratch\Config\UserConfig::class,
+                ]
+            )
+        );
     }
 }
